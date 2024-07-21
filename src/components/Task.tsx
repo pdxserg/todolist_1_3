ca45710546a339021a04c6cf3def6abb67f756f8
SergeyStarchenko <pdxserg@gmail.com>
2024-07-20 21:25:10 -0700

    refactor Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,21 +4,20 @@ import {EditableSpan} from "../EditableSpan";
 import {TaskType} from "../AppWithRedux";
 
 
-type TaskPropsType={
-	id:string
+type TaskPropsType = {
+	id: string
 	t: TaskType
 	dispatch: any
 }
- export const Task = React.memo( ({id,t,dispatch}: TaskPropsType) => {
+export const Task = React.memo(({id, t, dispatch}: TaskPropsType) => {
 
 
-
-	 const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		 dispatch(changeStatusAC(id, t.id, e.currentTarget.checked))
-	 }
-	 const calbackTaskHandler = useCallback((newTitle: string) => {
-		 dispatch(changeTaskTitleAC(id, t.id, newTitle))
-	 },[])
+	const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		dispatch(changeStatusAC(id, t.id, e.currentTarget.checked))
+	}
+	const calbackTaskHandler = useCallback((newTitle: string) => {
+		dispatch(changeTaskTitleAC(id, t.id, newTitle))
+	}, [dispatch, id, t.id])
 
 
 	return (
