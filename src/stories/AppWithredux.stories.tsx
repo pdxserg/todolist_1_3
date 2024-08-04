340a1e19a8210ae11c8308530fe6725fa003b299
SergeyStarchenko <pdxserg@gmail.com>
2024-08-03 23:12:31 -0700

    ReduxStoreProviderDecorator.tsx:

--- src/stories/AppWithredux.stories.tsx
+++ src/stories/AppWithredux.stories.tsx
@@ -2,14 +2,15 @@ import type {Meta, StoryObj} from '@storybook/react';
 import {Task} from "../components/Task";
 import {Provider} from "react-redux";
 import {store} from "../store/store";
+import AppWithRedux from "../AppWithRedux";
 
 
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-	title: 'TODOLIST/Task',
-	component: Task,
+	title: 'TODOLIST/AppWithRedux',
+	component: AppWithRedux,
 	parameters: {
 		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
 		layout: 'centered',
@@ -22,12 +23,12 @@ const meta = {
 	//
 	// }
 
-} satisfies Meta<typeof Task>;
+} satisfies Meta<typeof AppWithRedux>;
 
 export default meta;
-type Story = StoryObj<typeof Task>;
+type Story = StoryObj<typeof AppWithRedux>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/arg
-export const TaskIsNotDoneStory:Story= {
-	render: ()=><Provider store={store}><Task task={{id:"swsss",title:"JS", isDone: false}} todoId={"sss"}   /></Provider>
+export const AppWithReduxStory:Story= {
+	render: ()=><Provider store={store}><AppWithRedux /></Provider>
 }
